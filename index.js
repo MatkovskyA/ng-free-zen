e6d974b58d2e4b2e791cad1512d5abbd274ed082
MatkovskyA <a.matkovsky14@gmail.com>
2024-07-14 22:07:47 +0300

    created logic for modal menu and start to work with light/dark theme

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const selectArrow = document.querySelector("#arrow-icon")
+const selectArrow = document.querySelector("#arrow-icon");
 
 const showSelect = () => {
-  let selectHeader = document.querySelectorAll('.select__header');
-  let selectItem = document.querySelectorAll(".select__item");
+  const selectHeader = document.querySelectorAll('.select__header');
+  const selectItem = document.querySelectorAll(".select__item");
 
   selectHeader.forEach(item => {
-    item.addEventListener('click', selectToggle)
+    item.addEventListener('click', selectToggle);
   })
 
   selectItem.forEach(item => {
-    item.addEventListener('click', selectItemValue)
+    item.addEventListener('click', selectItemValue);
   })
 
   function selectToggle() {
@@ -19,12 +19,34 @@ const showSelect = () => {
 
   function selectItemValue() {
     let value = this.innerText;
-    let select = this.closest('.select')
-    let currentSelect = select.querySelector('.select__current')
+    let select = this.closest('.select');
+    let currentSelect = select.querySelector('.select__current');
     currentSelect.innerText = value;
     select.classList.remove("active");
   }
-
 }
 
-showSelect()
+showSelect();
+
+// Смена темы
+const changeThemeBtn = document.querySelector("#theme");
+const header = document.querySelector(".header")
+
+changeThemeBtn.addEventListener("click", () => {
+  const app = document.querySelector('.app');
+  app.classList.toggle("light");
+  header.classList.toggle("light");
+  app.classList.toggle('dark')
+  header.classList.toggle('dark')
+})
+
+// Меню бургер
+
+const burgerBtn = document.querySelector("#burger");
+
+burgerBtn.addEventListener("click", () => {
+  const burgerMenu = document.querySelector("#menu");
+  burgerMenu.classList.toggle("active");
+  burgerMenu.classList.toggle("hidden");
+  burgerBtn.classList.toggle("active");
+})
